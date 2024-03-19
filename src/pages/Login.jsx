36bb2f3db24ff495137e963fb9cfb65dfc7aa080
imlnr <imlnr2002@gmail.com>
2024-03-19 23:00:14 +0530

    final commit:

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+// Login.js
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import "../styles/Forms.css"
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
+function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        // Add login logic here
+        console.log('Login clicked');
+    };
+
+    return (
+        <div className="container">
+            <h2>Login</h2>
+            <form className='signup-form' onSubmit={handleLogin}>
+                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                <button type="submit">Login</button>
+            </form>
+            <Link to="/signup">Don't have an account? Signup here</Link>
+        </div>
+    );
 }
 
-export default Login
+export default Login;
