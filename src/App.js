18b0ba6e9cb58841f231ee39764b7258dc331a69
imlnr <imlnr2002@gmail.com>
2024-03-19 22:55:21 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Signup from './pages/Signup';
+import Allroutes from './Mainroutes/Allroutes';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Signup/>
+        {/* <Signup/> */}
+        <Allroutes />
       </div>
     </BrowserRouter>
   );
