242d2b1c7f3b2b701ece4e48834dbb748b92b6b3
imlnr <imlnr2002@gmail.com>
2024-03-20 22:50:36 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Allroutes from './Mainroutes/Allroutes';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Navbar/>
         <Allroutes />
       </div>
     </BrowserRouter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import '../styles/Navbar.css'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav class="navbar">
+      <div class="navbar-container container2">
+        <input type="checkbox" name="" id="" />
+        <div class="hamburger-lines">
+          <span class="line line1"></span>
+          <span class="line line2"></span>
+          <span class="line line3"></span>
+        </div>
+        <ul class="menu-items">
+          <li><Link to="#">Home</Link></li>
+          <li><Link to="#">About</Link></li>
+          <li><Link to="#">Category</Link></li>
+          <li><Link to="#">Menu</Link></li>
+          <li><Link to="#">Testimonial</Link></li>
+          <li><Link to="#">Contact</Link></li>
+        </ul>
+        <h1 class="logo">Navbar</h1>
+      </div>
+    </nav>
   )
 }
 
