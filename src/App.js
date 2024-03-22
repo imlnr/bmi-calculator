7b3021752feb83de27cb18cd0ed5aa6352fd40e4
imlnr <imlnr2002@gmail.com>
2024-03-22 09:48:58 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Allroutes from './Mainroutes/Allroutes';
 import Navbar from './components/Navbar';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar/>
-        <Allroutes />
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Allroutes />
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,4 +2,4 @@ import { applyMiddleware, legacy_createStore } from "redux";
 import { reducer } from "./reducer";
 import { thunk } from "redux-thunk";
 
-const store = legacy_createStore(reducer, applyMiddleware(thunk));
+export const store = legacy_createStore(reducer, applyMiddleware(thunk));
